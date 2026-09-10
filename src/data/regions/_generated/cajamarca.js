60a0b115a0c36ea62d5ef708b3bdbcff7b5d0995
root <mvcs_pnc_gfpg@viviendaext.pe>
2026-09-10 00:16:35 +0000

    Actualiza datos de regiones (2026) -- pipeline automático

--- src/data/regions/_generated/cajamarca.js
+++ src/data/regions/_generated/cajamarca.js
@@ -61,7 +61,7 @@ export default {
 
   conveniosCount: 6,
   conveniosVigentes: [
-    { entidad: 'Gobierno Regional', detail: 'hasta 10/10/2026' },
+    { entidad: 'Gobierno Regional', detail: 'vence 10/10/2026 (a un mes de caducar)' },
     { entidad: 'Municipalidad Provincial Jaen', detail: 'vence 18/09/2026 (a un mes de caducar)' },
     { entidad: 'Municipalidad Distrital Callayuc', detail: 'hasta 11/11/2026' },
     { entidad: 'Municipalidad Distrital San Juan', detail: 'hasta 17/11/2026' },
