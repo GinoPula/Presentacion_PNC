fbff22f942476f74eb4c267eb08d164bd1f0fdf4
root <mvcs_pnc_gfpg@viviendaext.pe>
2026-09-12 18:48:52 +0000

    Actualiza datos de regiones (2026) -- pipeline automático

--- src/data/mapaIntervenciones.js
+++ src/data/mapaIntervenciones.js
@@ -11639,7 +11639,7 @@ const mapaIntervenciones = {
     "fechaInicio": "08/08/2026",
     "fechaFin": "06/10/2026",
     "poblacion": 1000,
-    "volumen": 720,
+    "volumen": 740,
     "enlace": null
   },
   {
