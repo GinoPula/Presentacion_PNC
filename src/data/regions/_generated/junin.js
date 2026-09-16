baf569d160b4ae642d99cff2e8ed5451ab3f432a
root <mvcs_pnc_gfpg@viviendaext.pe>
2026-09-16 17:02:05 +0000

    Actualiza datos de regiones y Reporte Diario (2026) -- pipeline automático

--- src/data/regions/_generated/junin.js
+++ src/data/regions/_generated/junin.js
@@ -24,7 +24,7 @@ export default {
 
   programadasCols: ['provincia', 'distrito'],
   programadas: [
-    { provincia: 'Chanchamayo', distrito: 'Chanchamayo', cantidad: 5, metaVol: 46834.82, metaKm: 3.16, poblacion: 6860 },
+    { provincia: 'Chanchamayo', distrito: 'Chanchamayo', cantidad: 6, metaVol: 53772.84, metaKm: 5.05, poblacion: 8780 },
     { provincia: 'Concepcion', distrito: 'Concepcion', cantidad: 1, metaVol: 6500.0, metaKm: 0.15, poblacion: 1200 },
     { provincia: 'Concepcion', distrito: 'Matahuasi', cantidad: 1, metaVol: 5714.0, metaKm: 1.0, poblacion: 300 },
     { provincia: 'Huancayo', distrito: 'Pucara', cantidad: 1, metaVol: 16702.3, metaKm: 3.16, poblacion: 770 },
@@ -34,13 +34,14 @@ export default {
     { provincia: 'Jauja', distrito: 'Masma', cantidad: 2, metaVol: 14000.0, metaKm: 3.0, poblacion: 1200 },
     { provincia: 'Tarma', distrito: 'Tarma', cantidad: 1, metaVol: 7414.4, metaKm: 1.8, poblacion: 3200 },
   ],
-  programadasTotal: { cantidad: 14, metaVol: 124987.35, metaKm: 15.8, poblacion: 19470 },
+  programadasTotal: { cantidad: 15, metaVol: 131925.37, metaKm: 17.7, poblacion: 21390 },
 
   programadasDetalle: [
     { provincia: 'Jauja', distrito: 'Julcan', sector: 'Río Yacus', ficha: '021-2026-LD-P-JUN', descripcion: ' "LIMPIEZA, DESCOLMATACION Y CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO DEL CAUCE DEL RIO YACUS  DE LA  PROG. 0+000 A LA PROG. 0+540 AMBAS MARGENES, EN LA ZONA URBANA DEL DISTRITO DE JULCAN, PROVINCIA DE JAUJA, REGION JUNIN"-00              ', fechaInicio: '01/09/2026', fechaFin: '14/09/2026', metaVol: 8400.0, metaKm: 0.54, poblacion: 480 },
     { provincia: 'Concepcion', distrito: 'Matahuasi', sector: 'Zona Urbana', ficha: '003-2026-LD-P-JUN', descripcion: ' "LIMPIEZA, DESCOLMATACION, ENCAUZAMIENTO Y CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO DEL RIO SECO,  DE LA  PROG. 0+000 A LA PROG. 0+500, AMBAS MÁRGENES, EN LA ZONA URBANA DEL DISTRITO DE MATAHUASI, PROVINCIA DE CONCEPCIÓN, REGION JUNIN"-00                   ', fechaInicio: '05/09/2026', fechaFin: '18/09/2026', metaVol: 5714.0, metaKm: 1.0, poblacion: 300 },
     { provincia: 'Chanchamayo', distrito: 'Chanchamayo', sector: 'Zona Urbana', ficha: '002-2026-LD-P-JUN', descripcion: 'LIMPIEZA, DESCOLMATACION Y ENCAUZAMIENTO DEL RIO GAROU, PROG. 0+000 A LA PROG. 0+625.13 KM EN AMBAS MARGENES DE LA ZONA URBANA DE LA MERCED, DISTRITO Y PROVINCIA DE CHANCHAMAYO-REGION JUNIN', fechaInicio: '15/09/2026', fechaFin: '29/09/2026', metaVol: 8215.97, metaKm: 0.62, poblacion: 720 },
     { provincia: 'Tarma', distrito: 'Tarma', sector: 'Zona Urbana', ficha: '011-2026-LD-P-JUN', descripcion: ' "LIMPIEZA Y DESCOLMATACIÓN DEL CAUCE DEL RIO MANIACO AMBAS MÁRGENES, DE LA PROG. 0+000 A LA PROG. 1+820, EN LA ZONA URBANA DEL DISTRITO DE TARMA, PROVINCIA DE TARMA, REGIÓN JUNIN"-00                    ', fechaInicio: '18/09/2026', fechaFin: '01/10/2026', metaVol: 7414.4, metaKm: 1.8, poblacion: 3200 },
+    { provincia: 'Chanchamayo', distrito: 'Chanchamayo', sector: 'Zona Urbana', ficha: '068-2026-LD-P-JUN', descripcion: 'LIMPIEZA, DESCOLMATACION Y ENCAUZAMIENTO DEL RIO GAROU, PROG. 0+000 A LA PROG. 1+897.11 KM TRAMO III DE LAZONA URBANA DE LA MERCED, DISTRITO Y PROVINCIA DE CHANCHAMAYO-REGION JUNIN', fechaInicio: '01/10/2026', fechaFin: '15/10/2026', metaVol: 6938.02, metaKm: 1.9, poblacion: 1920 },
     { provincia: 'Chanchamayo', distrito: 'Chanchamayo', sector: 'Rio Quisque', ficha: '018-2026-LD-P-JUN', descripcion: '  "LIMPIEZA, DESCOLMATACION, ENCAUZAMIENTO Y CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO DEL RÍO QUISQUE,  DE LA  PROG. 0+000 A LA PROG. 0+354 AMBAS MÁRGENES, EN LA ZONA URBANA DEL DISTRITO DE CHANCHAMAYO, PROVINCIA DE CHANCHAMAYO, REGION JUNIN"-00                  ', fechaInicio: '05/10/2026', fechaFin: '18/10/2026', metaVol: 3500.0, metaKm: 0.35, poblacion: 300 },
     { provincia: 'Jauja', distrito: 'Masma', sector: 'Zona Urbana', ficha: '007-2026-LD-P-JUN', descripcion: ' LIMPIEZA, DESCOLMATACION Y CONFORMACION DE DIQUES CON MATERIAL PROPIO DEL CAUCE DEL RIO MASMA, DE LA PROG 0+000 A LA PROG 1+500, EN VARIOS SECTORES DEL DISTRITO DE MASMA, PROVINCIA DE JAUJA, REGION JUNIN-00    ', fechaInicio: '05/10/2026', fechaFin: '18/10/2026', metaVol: 7000.0, metaKm: 1.5, poblacion: 600 },
     { provincia: 'Concepcion', distrito: 'Concepcion', sector: 'Rio Achamayo', ficha: '024-2026-LD-P-JUN', descripcion: ' "LIMPIEZA, DESCOLMATACIÓN, ENCAUZAMIENTO Y CONFORMACIÓN DE DIQUES CON MATERIAL PROPIO EN EL RÍO ACHAMAYO, DE LA PROG 0+000 A LA PROG. 0+500, MARGEN DERECHA, DEL DISTRITO DE INGENIO, PROVINCIA DE CONCEPCION, DEPARTAMENTO JUNIN"               ', fechaInicio: '07/10/2026', fechaFin: '21/10/2026', metaVol: 6500.0, metaKm: 0.15, poblacion: 1200 },
