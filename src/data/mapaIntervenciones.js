13e0f84a2b958fec48613a71823022dbfe9af593
root <mvcs_pnc_gfpg@viviendaext.pe>
2026-09-25 15:16:42 +0000

    Actualiza datos de regiones y Reporte Diario (2026) -- pipeline automático

--- src/data/mapaIntervenciones.js
+++ src/data/mapaIntervenciones.js
@@ -9341,24 +9341,6 @@ const mapaIntervenciones = {
     "enlace": "https://drive.google.com/drive/folders/1E-2a2BArVIBLWbAAHlTWwlYHYUeZjDJB"
   },
   {
-    "id": 10007,
-    "lat": -12.625635,
-    "lng": -73.78998,
-    "estado": "Ejecutada",
-    "tipo": "Emergencia",
-    "provincia": "La Mar",
-    "distrito": "Ayna",
-    "sector": "Barrios Altos Y Palmeras",
-    "descripcion": " LIMPIEZA DESCOLMATACIÓN Y ELIMINACIÓN DE MATERIAL DEL CAUCE EN EL RÍO SANKIRHUATO, DEL DISTRITO DE AYNA SAN FRANCISCO, PROVINCIA DE LA MAR, DEPARTAMENTO DE AYACUCHO    ",
-    "ficha": "35-2026- LD-E-AYAC",
-    "fechaInicio": "11/03/2026",
-    "fechaFin": "02/04/2026",
-    "poblacion": 1000,
-    "volumen": 12710,
-    "km": 0.29,
-    "enlace": "https://drive.google.com/drive/folders/1E-2a2BArVIBLWbAAHlTWwlYHYUeZjDJB "
-  },
-  {
     "id": 9705,
     "lat": -13.188399,
     "lng": -74.21163,
@@ -9375,6 +9357,24 @@ const mapaIntervenciones = {
     "volumen": 2232,
     "km": 0.62,
     "enlace": "https://drive.usercontent.google.com/download?id=1nBnvjtDabrEN-X0EbGr130KdH-NonKGQ&authuser=0&acrobatPromotionSource=gdrive_chrome-list"
+  },
+  {
+    "id": 10007,
+    "lat": -12.625635,
+    "lng": -73.78998,
+    "estado": "Ejecutada",
+    "tipo": "Emergencia",
+    "provincia": "La Mar",
+    "distrito": "Ayna",
+    "sector": "Barrios Altos Y Palmeras",
+    "descripcion": " LIMPIEZA DESCOLMATACIÓN Y ELIMINACIÓN DE MATERIAL DEL CAUCE EN EL RÍO SANKIRHUATO, DEL DISTRITO DE AYNA SAN FRANCISCO, PROVINCIA DE LA MAR, DEPARTAMENTO DE AYACUCHO    ",
+    "ficha": "35-2026- LD-E-AYAC",
+    "fechaInicio": "11/03/2026",
+    "fechaFin": "02/04/2026",
+    "poblacion": 1000,
+    "volumen": 12710,
+    "km": 0.29,
+    "enlace": "https://drive.google.com/drive/folders/1E-2a2BArVIBLWbAAHlTWwlYHYUeZjDJB "
   },
   {
     "id": 10040,
@@ -9626,6 +9626,24 @@ const mapaIntervenciones = {
     "poblacion": 78,
     "volumen": 1170,
     "km": 0.195,
+    "enlace": null
+  },
+  {
+    "id": 10461,
+    "lat": -13.318922,
+    "lng": -74.341322,
+    "estado": "En ejecución",
+    "tipo": "Prevención",
+    "provincia": "Huamanga",
+    "distrito": "Vinchos",
+    "sector": "Arizona",
+    "descripcion": "ENCAUSAMIENTO Y REFORZAMIENTO DE DIQUE EN EL RÍO CACHI MARGEN DERECHO, TRAMO II; EN EL CENTRO POBLADO DE ARIZONA EN EL DISTRITO DE VINCHOS, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO.    ",
+    "ficha": "42-2025-LD-PI-AYAC",
+    "fechaInicio": "26/09/2026",
+    "fechaFin": "10/10/2026",
+    "poblacion": 130,
+    "volumen": 5625,
+    "km": 0.6,
     "enlace": null
   }
 ],
