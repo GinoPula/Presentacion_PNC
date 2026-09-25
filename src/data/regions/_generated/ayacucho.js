d4c37adcc65e018524b42b7926e76cba4ae59ac0
root <mvcs_pnc_gfpg@viviendaext.pe>
2026-09-25 22:25:05 +0000

    Actualiza datos de regiones y Reporte Diario (2026) -- pipeline automático

--- src/data/regions/_generated/ayacucho.js
+++ src/data/regions/_generated/ayacucho.js
@@ -2,10 +2,10 @@
 // Este archivo se sobreescribe completo en cada corrida del pipeline de datos.
 export default {
   ejecutadasPorTipo: [
-    { tipo: 'Emergencia', cantidad: 8, m3: 59152.0, km: 2.5, poblacion: 6828, provincias: ['Huamanga', 'Huanta', 'La Mar'] },
-    { tipo: 'Prevención', cantidad: 16, m3: 36584.44, km: 4.85, poblacion: 10094, provincias: ['Huamanga', 'Huanta', 'Vilcas Huaman'] },
+    { tipo: 'Emergencia', cantidad: 8, m3: 59152.0, km: 2.5, poblacion: 10076, provincias: ['Huamanga', 'Huanta', 'La Mar'] },
+    { tipo: 'Prevención', cantidad: 16, m3: 36584.44, km: 4.85, poblacion: 10119, provincias: ['Huamanga', 'Huanta', 'Vilcas Huaman'] },
   ],
-  ejecutadasTotal: { cantidad: 24, m3: 95736.44, m3AguaPotable: 0.0, km: 7.35, poblacion: 16922 },
+  ejecutadasTotal: { cantidad: 24, m3: 95736.44, m3AguaPotable: 0.0, km: 7.35, poblacion: 20195 },
 
   anioAnterior: '2025',
   ejecutadasPorTipoAnioAnterior: [
@@ -20,29 +20,27 @@ export default {
 
   programadasCols: ['provincia', 'distrito'],
   programadas: [
-    { provincia: 'Huamanga', distrito: 'Ayacucho', cantidad: 2, metaVol: 1440.0, metaKm: 0.46, poblacion: 1320 },
-    { provincia: 'Huamanga', distrito: 'Jesus Nazareno', cantidad: 1, metaVol: 1704.0, metaKm: 0.35, poblacion: 200 },
+    { provincia: 'Huamanga', distrito: 'Ayacucho', cantidad: 1, metaVol: 840.0, metaKm: 0.28, poblacion: 320 },
+    { provincia: 'Huamanga', distrito: 'Jesus Nazareno', cantidad: 1, metaVol: 2700.0, metaKm: 0.18, poblacion: 200 },
     { provincia: 'Huamanga', distrito: 'Quinua', cantidad: 1, metaVol: 2496.0, metaKm: 0.26, poblacion: 158 },
+    { provincia: 'Huamanga', distrito: 'San Juan Bautista', cantidad: 1, metaVol: 540.0, metaKm: 0.1, poblacion: 500 },
     { provincia: 'Huamanga', distrito: 'Santiago De Pischa', cantidad: 1, metaVol: 5250.0, metaKm: 0.32, poblacion: 40 },
     { provincia: 'Huamanga', distrito: 'Vinchos', cantidad: 3, metaVol: 23469.0, metaKm: 2.54, poblacion: 519 },
-    { provincia: 'Huanta', distrito: 'Sivia', cantidad: 2, metaVol: 7500.0, metaKm: 1.0, poblacion: 413 },
     { provincia: 'La Mar', distrito: 'Santa Rosa', cantidad: 2, metaVol: 6090.0, metaKm: 0.41, poblacion: 450 },
   ],
-  programadasTotal: { cantidad: 12, metaVol: 47949.0, metaKm: 5.34, poblacion: 3100 },
+  programadasTotal: { cantidad: 10, metaVol: 41385.0, metaKm: 4.09, poblacion: 2187 },
 
   programadasDetalle: [
     { provincia: 'Huamanga', distrito: 'Vinchos', sector: 'Arizona', ficha: '42-2025-LD-PI-AYAC', descripcion: 'ENCAUSAMIENTO Y REFORZAMIENTO DE DIQUE EN EL RÍO CACHI MARGEN DERECHO, TRAMO II; EN EL CENTRO POBLADO DE ARIZONA EN EL DISTRITO DE VINCHOS, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO.        ', fechaInicio: '25/09/2026', fechaFin: '09/10/2026', metaVol: 5625.0, metaKm: 0.6, poblacion: 130 },
     { provincia: 'Huamanga', distrito: 'Santiago De Pischa', sector: 'Anyana', ficha: '43-2026- LD-PI-AYAC ', descripcion: ' LIMPIEZA, DESCOLMATACIÓN Y CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO EN EL MARGEN DERECHO DEL RIO CACHI TRAMO III, ANEXO DE ANYANA, DISTRITO DE SANTIAGO DE PISCHA, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO        ', fechaInicio: '06/10/2026', fechaFin: '20/10/2026', metaVol: 5250.0, metaKm: 0.32, poblacion: 40 },
     { provincia: 'Huamanga', distrito: 'Quinua', sector: 'Chacco', ficha: '07-2026- LD-PI-AYAC', descripcion: 'LIMPIEZA, DESCOLMATACION Y ELIMINACIÓN DE MATERIAL EXCEDENTE DEL CAUCE DEL RIO YUCAES DEL SECTOR CHACCO DEL DISTRITO DE QUINUA, PROVINCIA DE HUAMANGA, REGION AYACUCHO-00                ', fechaInicio: '22/10/2026', fechaFin: '05/11/2026', metaVol: 2496.0, metaKm: 0.26, poblacion: 158 },
-    { provincia: 'Huamanga', distrito: 'Vinchos', sector: 'Pacclarumi', ficha: '05-2026-LD-PI-AYAC', descripcion: 'LIMPIEZA, DESCOLMATACIÓN Y CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO EN EL RIO CACHI TRAMO III EN EL SECTOR PACCLARUMI, UBICADO EN ANEXO SAN LUIS DE PICHA EN EL DISTRITO DE VINCHOS, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO.-00         ', fechaInicio: '02/11/2026', fechaFin: '13/11/2026', metaVol: 7644.0, metaKm: 0.24, poblacion: 144 },
+    { provincia: 'Huamanga', distrito: 'San Juan Bautista', sector: 'Chaquihuaycco', ficha: '45-2026-LD-E-AYAC', descripcion: ' LIMPIEZA, DESCOLMATACIÓN Y ELIMINACIÓN DE MATERIAL EXCEDENTE DEL CAUCE DE LA QUEBRADA CHAQUIHUAYCCO (TRAMO V ), EN EL DISTRITO DE SAN JUAN BAUTISTA, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO    ', fechaInicio: '22/10/2026', fechaFin: '28/10/2026', metaVol: 540.0, metaKm: 0.1, poblacion: 500 },
+    { provincia: 'Huamanga', distrito: 'Jesus Nazareno', sector: 'Huichccana Chico', ficha: '08-2026-LD-PI-AYAC  ', descripcion: 'LIMPIEZA, DESCOLMATACIÓN Y ELIMINACIÓN DE MATERIAL EXCEDENTE DEL CAUCE DE LA QUEBRADA SAN CARLOS TRAMO I, EN EL SECTOR HUICHCCANA DEL DISTRITO DE JESÚS NAZARENO, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO-00         ', fechaInicio: '28/10/2026', fechaFin: '03/11/2026', metaVol: 2700.0, metaKm: 0.18, poblacion: 200 },
     { provincia: 'Huamanga', distrito: 'Ayacucho', sector: 'Wari Accopampa', ficha: '44-2026- LD-PI-AYAC ', descripcion: ' LIMPIEZA, DESCOLMATACION Y ELIMINACION DE MATERIAL EXCEDENTE DEL CAUCE DE LA QUEBRADA ACCO ACCO TRAMO II, EN EL SECTOR WARI ACCOPAMPA DEL DISTRITO DE AYACUCHO, PROVINCIA DE HUAMANGA, REGION AYACUCHO     ', fechaInicio: '02/11/2026', fechaFin: '12/11/2026', metaVol: 840.0, metaKm: 0.28, poblacion: 320 },
-    { provincia: 'Huamanga', distrito: 'Jesus Nazareno', sector: 'San Carlos', ficha: '08-2026-LD-PI-AYAC  ', descripcion: 'LIMPIEZA, DESCOLMATACIÓN Y ELIMINACIÓN DE MATERIAL EXCEDENTE DEL CAUCE DE LA QUEBRADA SAN CARLOS TRAMO I, EN EL SECTOR HUICHCCANA DEL DISTRITO DE JESÚS NAZARENO, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO-00        ', fechaInicio: '05/11/2026', fechaFin: '11/11/2026', metaVol: 1704.0, metaKm: 0.35, poblacion: 200 },
+    { provincia: 'Huamanga', distrito: 'Vinchos', sector: 'Pacclarumi', ficha: '05-2026-LD-PI-AYAC', descripcion: 'LIMPIEZA, DESCOLMATACIÓN Y CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO EN EL RIO CACHI TRAMO III EN EL SECTOR PACCLARUMI, UBICADO EN ANEXO SAN LUIS DE PICHA EN EL DISTRITO DE VINCHOS, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO.-00         ', fechaInicio: '02/11/2026', fechaFin: '13/11/2026', metaVol: 7644.0, metaKm: 0.24, poblacion: 144 },
     { provincia: 'La Mar', distrito: 'Santa Rosa', sector: 'Puente', ficha: '13-2026- LD-PI-AYAC', descripcion: 'LIMPIEZA, DESCOLMATACIÓN, CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO Y ELIMINACIÓN DE MATERIAL EXCEDENTE DEL CAUCE DEL RÍO SANTA ROSA EN EL TRAMO III DEL SECTOR PUENTE DEL DISTRITO DE SANTA ROSA, PROVINCIA DE LA MAR, REGIÓN AYACUCHO-00                   ', fechaInicio: '04/12/2026', fechaFin: '09/12/2026', metaVol: 2415.0, metaKm: 0.16, poblacion: 200 },
     { provincia: 'La Mar', distrito: 'Santa Rosa', sector: 'Los Licenciados', ficha: '14-2026- LD-PI-AYAC ', descripcion: 'LIMPIEZA, DESCOLMATACION, CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO Y ELIMINACIÓN DE MATERIAL EXCEDENTE DEL CAUCE DEL RIO SANTA ROSA EN EL TRAMO IV, DEL SECTOR LOS LICENCIADOS DEL DISTRITO DE SANTA ROSA, PROVINCIA DE LA MAR, REGION AYACUCHO-00                  ', fechaInicio: '10/12/2026', fechaFin: '22/12/2026', metaVol: 3675.0, metaKm: 0.24, poblacion: 250 },
-    { provincia: 'Huanta', distrito: 'Sivia', sector: 'Zoológico', ficha: '18-2026- LD-PI-AYAC ', descripcion: 'LIMPIEZA, DESCOLMATACIÓN, ENCAUZAMIENTO Y REFORZAMIENTO DE DIQUES CON MATERIAL PROPIO Y ELIMINACIÓN DE MATERIAL DESCOLMATADO EN EL RIO QUIPIASARI, EN EL SECTOR ZOOLÓGICO DEL DISTRITO DE SIVIA, PROVINCIA DE HUANTA, REGIÓN AYACUCHO-00        ', fechaInicio: '12/12/2026', fechaFin: '23/12/2026', metaVol: 4620.0, metaKm: 0.55, poblacion: 200 },
     { provincia: 'Huamanga', distrito: 'Vinchos', sector: '', ficha: '06-2026-LD-P-AYAC', descripcion: 'LIMPIEZA, DESCOLMATACIÓN Y CONFORMACIÓN DE DIQUE CON MATERIAL PROPIO EN EL RIO CACHI TRAMO IV, UBICADO EN EL CENTRO POBLADO ANCHAC HUASI, EN EL DISTRITO DE VINCHOS, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO.      ', fechaInicio: '14/12/2026', fechaFin: '29/12/2026', metaVol: 10200.0, metaKm: 1.7, poblacion: 245 },
-    { provincia: 'Huamanga', distrito: 'Ayacucho', sector: 'Centro Capital', ficha: '11-2026- LD-PI-AYAC', descripcion: 'LIMPIEZA, DESCOLMATACIÓN Y ELIMINACIÓN DEL MATERIAL EXCEDENTE DEL CAUCE DE LA QUEBRADA ARROLLO SECO, EN EL DISTRITO DE AYACUCHO, PROVINCIA DE HUAMANGA, REGIÓN AYACUCHO.-00   ', fechaInicio: '15/12/2026', fechaFin: '19/12/2026', metaVol: 600.0, metaKm: 0.18, poblacion: 1000 },
-    { provincia: 'Huanta', distrito: 'Sivia', sector: 'C.P. San Juan De Matucana', ficha: '21-2026- LD-PI-AYAC ', descripcion: 'LIMPIEZA, DESCOLMATACIÓN, ENCAUZAMIENTO, CONFORMACIÓN DE DIQUES CON MATERIAL PROPIO EN LA QUEBRADA PISHCCANA, EN EL CENTRO POBLADO DE SAN JUAN DE MATUCANA DEL DISTRITO DE SIVIA PROVINCIA DE HUANTA, REGIÓN AYACUCHO-00       ', fechaInicio: '15/12/2026', fechaFin: '29/12/2026', metaVol: 2880.0, metaKm: 0.45, poblacion: 213 },
   ],
 
   conveniosCount: 17,
--- src/data/regions/_generated/huancavelica.js
+++ src/data/regions/_generated/huancavelica.js
@@ -17,10 +17,12 @@ export default {
 
   programadasCols: ['provincia', 'distrito'],
   programadas: [
+    { provincia: 'Angaraes', distrito: 'San Antonio De Antaparco', cantidad: 1, metaVol: 1240.0, metaKm: 0.31, poblacion: 0 },
   ],
-  programadasTotal: { cantidad: 0, metaVol: 0, metaKm: 0, poblacion: 0 },
+  programadasTotal: { cantidad: 1, metaVol: 1240.0, metaKm: 0.31, poblacion: 0 },
 
   programadasDetalle: [
+    { provincia: 'Angaraes', distrito: 'San Antonio De Antaparco', sector: 'Antaparco', ficha: '46-2026-LD-PI-AYAC                                                                                                                                                                                                                       ', descripcion: ' LIMPIEZA, DESCOLMATACIÓN  DE MATERIAL EXCEDENTE DEL CAUCE DE LA QUEBRADA CHAQUI HUAYCCO, EN EL SECTOR ANTAPARCO DEL DISTRITO DE  SAN ANTONIO DE ANTAPARCO, PROVINCIA DE ANGARAES, REGIÓN HUANCAVELICA', fechaInicio: '05/10/2026', fechaFin: '10/10/2026', metaVol: 1240.0, metaKm: 0.31, poblacion: null },
   ],
 
   conveniosCount: 3,
